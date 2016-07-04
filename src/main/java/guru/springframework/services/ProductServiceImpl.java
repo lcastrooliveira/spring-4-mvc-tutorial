57b91e312443bf8d050d87bfd802aa4403857a68
Lucas de Castro Oliveira <lcastro.oliveira@gmail.com>
2016-07-04 00:06:38 -0300

    Create Display single product template and controller method

--- src/main/java/guru/springframework/services/ProductServiceImpl.java
+++ src/main/java/guru/springframework/services/ProductServiceImpl.java
@@ -68,4 +68,9 @@ public class ProductServiceImpl implements ProductService {
 		return new ArrayList<>(products.values());
 	}
 
+	@Override
+	public Product getProductById(Integer id) {
+		return products.get(id);
+	}
+
 }
