baa700f43e041155ce2b75fb9e75bc9260db057d
Lucas de Castro Oliveira <lcastro.oliveira@gmail.com>
2016-07-12 22:32:33 -0300

    Introduction to JPA with H2 Database

--- src/main/java/guru/springframework/SpringmvcApplication.java
+++ src/main/java/guru/springframework/SpringmvcApplication.java
@@ -1,12 +1,19 @@
 package guru.springframework;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringmvcApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringmvcApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(SpringmvcApplication.class, args);
+//		System.out.println("Beans ********");
+//		System.out.println(ctx.getBeanDefinitionCount());
+//		for(String name : ctx.getBeanDefinitionNames()) {
+//			System.out.println(name);
+//		}
 	}
 }
