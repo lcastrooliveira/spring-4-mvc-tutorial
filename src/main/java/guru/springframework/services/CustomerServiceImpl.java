2d01e9917b2d1d1af51932049de749fb16c5089c
Lucas de Castro Oliveira <lcastro.oliveira@gmail.com>
2016-07-14 01:37:08 -0300

    Add CustomerSerivce JpaDaoImpl and its test class

--- src/main/java/guru/springframework/services/CustomerServiceImpl.java
+++ src/main/java/guru/springframework/services/CustomerServiceImpl.java
@@ -6,64 +6,31 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
+
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import guru.springframework.domain.Customer;
 
 @Service
+@Profile("map")
 public class CustomerServiceImpl implements CustomerService {
 
-	private Map<Integer, Customer> customers;
+	@PersistenceUnit
+	private EntityManagerFactory emf;
 	
-	public CustomerServiceImpl() {
-		loadAllCustomers();
-	}
+	private Map<Integer, Customer> customers;
 	
 	public void loadAllCustomers() {
 		customers = new HashMap<>();
-		
-		Customer customer1 = new Customer();
-		customer1.setId(1);
-		customer1.setAddressLine1("Rua A, 123");
-		customer1.setAddressLine2("Jardim dos Cachorros");
-		customer1.setCity("Foz do Iguaçu");
-		customer1.setEmail("fernando@gmail.com");
-		customer1.setFirstName("Fernando");
-		customer1.setLastName("Fagundes");
-		customer1.setPhoneNumber("+554152368987");
-		customer1.setState("Paraná");
-		customer1.setZipCode("85847985");
-		
-		customers.put(1, customer1);
-		
-		Customer customer2 = new Customer();
-		customer2.setId(2);
-		customer2.setAddressLine1("Rua B, 456");
-		customer2.setAddressLine2("Jardim dos Gatos");
-		customer2.setCity("Cascavel");
-		customer2.setEmail("luciana@gmail.com");
-		customer2.setFirstName("Luciana");
-		customer2.setLastName("Teixeira");
-		customer2.setPhoneNumber("+554563215478");
-		customer2.setState("Paraná");
-		customer2.setZipCode("85847325");
-		
-		customers.put(2, customer2);
-		
-		Customer customer3 = new Customer();
-		customer3.setId(3);
-		customer3.setAddressLine1("Rua C, 789");
-		customer3.setAddressLine2("Jardim dos Coelhos");
-		customer3.setCity("Recife");
-		customer3.setEmail("Alberto@gmail.com");
-		customer3.setFirstName("Alberto");
-		customer3.setLastName("Barroso");
-		customer3.setPhoneNumber("+556523215852");
-		customer3.setState("Pernambuco");
-		customer3.setZipCode("45632456");
-		
-		customers.put(3, customer3);
-		
+		EntityManager em = emf.createEntityManager();
+		List<Customer> loadedCustomers = em.createQuery("from Customer", Customer.class).getResultList();
+		for(Customer customer : loadedCustomers) {
+			customers.put(customer.getId(), customer);
+		}
 	}
 	
 	@Override
--- src/main/java/guru/springframework/services/ProductServiceImpl.java
+++ src/main/java/guru/springframework/services/ProductServiceImpl.java
@@ -1,11 +1,14 @@
 package guru.springframework.services;
 
-import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
 
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
@@ -16,6 +19,9 @@ import guru.springframework.domain.Product;
 @Profile("map")
 public class ProductServiceImpl implements ProductService {
 
+	@PersistenceUnit
+	private EntityManagerFactory emf;
+	
 	private Map<Integer, Product> products;
 
 	public ProductServiceImpl() {
@@ -25,45 +31,11 @@ public class ProductServiceImpl implements ProductService {
 	private void loadAllProducts() {
 		products = new HashMap<>();
 
-		Product product1 = new Product();
-		product1.setId(1);
-		product1.setDescription("Product 1");
-		product1.setPrice(new BigDecimal("12.99"));
-		product1.setImageUrl("http://example.com/product1");
-
-		products.put(1, product1);
-
-		Product product2 = new Product();
-		product2.setId(2);
-		product2.setDescription("Product 2");
-		product2.setPrice(new BigDecimal("14.99"));
-		product2.setImageUrl("http://example.com/product2");
-
-		products.put(2, product2);
-
-		Product product3 = new Product();
-		product3.setId(3);
-		product3.setDescription("Product 3");
-		product3.setPrice(new BigDecimal("34.99"));
-		product3.setImageUrl("http://example.com/product3");
-
-		products.put(3, product3);
-
-		Product product4 = new Product();
-		product4.setId(4);
-		product4.setDescription("Product 4");
-		product4.setPrice(new BigDecimal("44.99"));
-		product4.setImageUrl("http://example.com/product4");
-
-		products.put(4, product4);
-
-		Product product5 = new Product();
-		product5.setId(5);
-		product5.setDescription("Product 5");
-		product5.setPrice(new BigDecimal("25.99"));
-		product5.setImageUrl("http://example.com/product5");
-
-		products.put(5, product5);
+		EntityManager em = emf.createEntityManager();
+		List<Product> loadedProducts = em.createQuery("from Product", Product.class).getResultList();
+		for(Product product : loadedProducts) {
+			products.put(product.getId(), product);
+		}
 	}
 
 	@Override
