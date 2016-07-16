60a4adbba2588479d4411d9fcf31907fa628995c
Lucas de Castro Oliveira <lcastro.oliveira@gmail.com>
2016-07-16 12:22:21 -0300

    Add AbstractionDomain class for common fields and Order and OrderLine class

--- src/main/java/guru/springframework/SpringmvcApplication.java
+++ src/main/java/guru/springframework/SpringmvcApplication.java
@@ -3,13 +3,13 @@ package guru.springframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringmvcApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext ctx = SpringApplication.run(SpringmvcApplication.class, args);
+//		ApplicationContext ctx = 
+		SpringApplication.run(SpringmvcApplication.class, args);
 //		System.out.println("Beans ********");
 //		System.out.println(ctx.getBeanDefinitionCount());
 //		for(String name : ctx.getBeanDefinitionNames()) {
--- src/main/java/guru/springframework/domain/Customer.java
+++ src/main/java/guru/springframework/domain/Customer.java
@@ -1,15 +1,16 @@
 package guru.springframework.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
-import javax.persistence.Version;
 
 @Entity
-public class Customer {
+public class Customer extends AbstractDomainObject {
 
 	private String firstName, lastName, email, phoneNumber;
 	
@@ -19,47 +20,11 @@ public class Customer {
 	@Embedded
 	private Address shippingAddress;
 	
-	public Address getBillingAddress() {
-		return billingAddress;
-	}
-
-	public void setBillingAddress(Address billingAddress) {
-		this.billingAddress = billingAddress;
-	}
-
-	public Address getShippingAddress() {
-		return shippingAddress;
-	}
-
-	public void setShippingAddress(Address shippingAddress) {
-		this.shippingAddress = shippingAddress;
-	}
-
-	@OneToOne
+	@OneToOne(cascade = {CascadeType.MERGE,CascadeType.PERSIST})
 	private User user;
 	
-	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	private Integer id;
-	
-	@Version
-	private Integer version;
-	
-	public Integer getVersion() {
-		return version;
-	}
-
-	public void setVersion(Integer version) {
-		this.version = version;
-	}
-	
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "customer", orphanRemoval = true)
+	private List<Order> orders = new ArrayList<>();
 
 	public String getFirstName() {
 		return firstName;
@@ -99,5 +64,29 @@ public class Customer {
 
 	public void setUser(User user) {
 		this.user = user;
+	}
+	
+	public Address getBillingAddress() {
+		return billingAddress;
+	}
+
+	public void setBillingAddress(Address billingAddress) {
+		this.billingAddress = billingAddress;
+	}
+
+	public Address getShippingAddress() {
+		return shippingAddress;
+	}
+
+	public void setShippingAddress(Address shippingAddress) {
+		this.shippingAddress = shippingAddress;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
+	}
+
+	public void setOrders(List<Order> orders) {
+		this.orders = orders;
 	}
 }
